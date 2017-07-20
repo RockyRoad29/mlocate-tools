bc39864801da527e5dcaf319cfbf9b58d75b8242
Michelle Baert <rocky.road@rocky-shore.net>
2017-07-20 09:24:19 +0200

    limits, filtering

--- filter.py
+++ filter.py
@@ -10,16 +10,26 @@ Parses and filter an mlocate database.
 
 >>> args = arg_parser().parse_args(['--show-config'])
 >>> args
-Namespace(database='/var/lib/mlocate/mlocate.db', log_level='WARNING', regexps=[], show_config=True)
+Namespace(database='/var/lib/mlocate/mlocate.db', limit_input=-1, limit_output=-1, log_level='WARNING', regexps=[], show_config=True)
 >>> run(args)
 database        : /var/lib/mlocate/mlocate.db
+limit_input     : -1
+limit_output    : -1
 log_level       : WARNING
 regexps         : []
 show_config     : True
 
+Filter directories which contain an entry matching some of the given regexps
+>>> args = arg_parser().parse_args('-d /tmp/MyBook.db -M 10 .*\.ini'.split())
+>>> run(args)
+2013-08-16 17:03:59.956254 /run/media/mich/MyBook/$RECYCLE.BIN/S-1-5-21-1696441804-2191777423-1598828944-1001
+
 """
 import logging
 import argparse
+import re
+import mlocate
+import json
 
 MLOCATE_DEFAULT_DB = "/var/lib/mlocate/mlocate.db"
 logging.basicConfig(level='DEBUG')
@@ -31,20 +41,26 @@ def arg_parser():
 
     >>> parser = arg_parser()
 
+    >>> parser.parse_args('.*\\.ini .*\\.desktop'.split()) # doctest: +ELLIPSIS
+    Namespace(...database='/var/lib/mlocate/mlocate.db', ... regexps=['.*\\\\.ini', '.*\\\\.desktop']...)
 
     You can specify an alternate database
-    >>> parser.parse_args('-d /tmp/MyBook.db'.split())
-    Namespace(database='/tmp/MyBook.db', log_level='WARNING', regexps=[], show_config=False)
-    >>> parser.parse_args('--log-level CRITICAL'.split())
-    Namespace(database='/var/lib/mlocate/mlocate.db', log_level='CRITICAL', regexps=[], show_config=False)
+    >>> parser.parse_args('-d /tmp/MyBook.db'.split()) # doctest: +ELLIPSIS
+    Namespace(database='/tmp/MyBook.db', ...)
+
+
+    adjust the logging level
+    >>> parser.parse_args('--log-level CRITICAL'.split()) # doctest: +ELLIPSIS
+    Namespace(...log_level='CRITICAL'...)
 
     Dry-run, show config only
-    >>> parser.parse_args('--show-config'.split())
-    Namespace(database='/var/lib/mlocate/mlocate.db', log_level='WARNING', regexps=[], show_config=True)
+    >>> parser.parse_args('--show-config'.split()) # doctest: +ELLIPSIS
+    Namespace(...show_config=True...)
 
     This doesn't work: >>> parser.parse_args(['--help'])
     >>> parser.print_help()
-    usage: docrunner.py [-h] [-L LOG_LEVEL] [-C] [-d DATABASE]
+    usage: docrunner.py [-h] [-L LOG_LEVEL] [-C] [-d DATABASE] [-M LIMIT_INPUT]
+                        [-m LIMIT_OUTPUT]
                         [regexps [regexps ...]]
     <BLANKLINE>
     Lookup items in mlocate database
@@ -58,17 +74,22 @@ def arg_parser():
       -C, --show-config     Dry run, only show current configuration
       -d DATABASE, --database DATABASE
                             name of the mlocate database
+      -M LIMIT_INPUT, --limit-input LIMIT_INPUT
+                            Maximum directory entries read from db
+      -m LIMIT_OUTPUT, --limit-output LIMIT_OUTPUT
+                            Maximum count of selected directories
 
-
-
-    """
+   """
     parser = argparse.ArgumentParser()
     parser.description = "Lookup items in mlocate database"
     #parser.add_argument('--verbose', '-v', action='count')
     parser.add_argument('-L', '--log-level', default='WARNING')
     parser.add_argument('-C', '--show-config', action='store_true', help="Dry run, only show current configuration")
     parser.add_argument('-d', '--database', help="name of the mlocate database", default=MLOCATE_DEFAULT_DB)
+    parser.add_argument('-M', '--limit-input', help="Maximum directory entries read from db", type=int, default=-1)
+    parser.add_argument('-m', '--limit-output', help="Maximum count of selected directories", type=int, default=-1)
     parser.add_argument('regexps', nargs='*',help="filtering regexp for filename/dirname")
+
     return parser
 
 def log_level(args):
@@ -77,6 +98,28 @@ def log_level(args):
 def show_config(args):
     for k,v in sorted(args.__dict__.items()):
         print("{0:15} : {1}".format(k, v))
+
+def do_filter(mdb, args):
+    """
+    :param mdb:
+    :param args:
+    :return:
+    """
+    selectors = [re.compile(r) for r in args.regexps ]
+    count = args.limit_output
+    for d in mdb.load_dirs(args.limit_input):
+        if match_dir(d, selectors):
+            print("{0} {1}".format(d['dt'], d['name']))
+            count -= 1
+            if count <= 0:
+                break
+
+def match_dir(d,  selectors):
+    for f in d['contents']:
+        for s in selectors:
+            if s.match(f[1]):
+                return True
+    return False
 
 def run(args):
     """
@@ -87,8 +130,12 @@ def run(args):
     logger.info("Running with %r", args)
     if args.show_config:
         show_config(args)
+        # TODO show mdb config
     else:
-        print("Not implemented")
+        mdb = mlocate.MLocateDB()
+        mdb.connect(args.database)
+        do_filter(mdb, args)
+
 
 if __name__ == '__main__':
     import argparse, sys
